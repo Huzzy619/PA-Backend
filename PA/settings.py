a6255ab157bb5ede33d19a7af0bf51ff350dacaf
Huzzy619 <blazingkrane@gmail.com>
2023-07-08 08:43:29 +0100

    update allowed hosts

--- PA/settings.py
+++ PA/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-3o3n569em9!b%zf$r9w=lvd-%#r(cs*fi+b+9-(^6ysudv)^@!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
